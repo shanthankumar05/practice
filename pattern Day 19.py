705e19d644c0aa96673ae4c31489b0ffc7b7d2bc
S.Shanthan Kumar <88592536+shanthankumar05@users.noreply.github.com>
2023-04-19 13:05:45 +0530

    Update pattern Day 19.py

--- pattern Day 19.py
+++ pattern Day 19.py
@@ -32,4 +32,53 @@ if(temp==num):
           print("Armstrong number");
     else:
           print("Not an armstrong number")
+          
+ ---Areas
+    
+   base =int(input("Enter base : ");
+   height =int(input("Enter height  : ");
+   side = int(input("Enter side length: ");
+   length= int(input("Enter length : ");
+   breadth = int(input("Enter breadth: ");
+   print("Area of triangle ",0.5*base*height)
+   print("Area of sqaure is ",side*side)
+   print("Area of rectangle ",length*breadth)
+  ---swapping
+   
+   n1=int(input("Enter value: ")
+n2=int(input("Enter value: ")
+n3=int(input("Enter value: ")
+
+
+if n1>n2 and n1>n3:
+    if n2<n3:
+         temp=n1
+        n1=n2
+        n2=n3
+        n3=temp
+    else:
+        temp=n1;
+        n1=n3
+        n3=temp
+elif n2>n1 and n2>n3:
+    if n1<n3:
+        temp=n2;
+        n1=n3
+        n3=temp
+    else:
+        temp=n2;
+        n2=n1
+        n3=temp
+else:
+    if n1>n2:
+        temp=n2
+        n2=n1
+        n1=temp
+print(n1,n2,n3)
+          
+    
+         
+        
+  
+             
            
